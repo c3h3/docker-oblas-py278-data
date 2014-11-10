cc2d6b8cc18dd2c33e105333a0e422987e28a552
c3h3 <c3h3.tw@gmail.com>
2014-11-11 02:32:32 +0800

    add password by env variable

--- ipython_notebook_config.py
+++ ipython_notebook_config.py
@@ -9,3 +9,13 @@ c.NotebookApp.allow_credentials = True
 c.NotebookApp.webapp_settings = {'headers': headers}
 
 
+import os
+password = os.environ.get("PASSWORD", "ipynb")
+
+from IPython.lib import passwd
+password = passwd(password)
+
+c.NotebookApp.password = unicode(password)
+
+
+
